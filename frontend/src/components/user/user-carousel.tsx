a1d149f8f72bcb4adba1b6fc821d7e89589b3502
DuHNunes <duhnunes.dev@gmail.com>
2025-04-10 00:43:08 -0300

    chore: Add prettier and simple-import-sort

--- frontend/src/components/user/user-carousel.tsx
+++ frontend/src/components/user/user-carousel.tsx
@@ -1,28 +1,33 @@
-"use client"
+'use client'
 
-import { Card, CardContent } from "../ui/card";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel";
+import { Card, CardContent } from '../ui/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '../ui/carousel'
 
-import Autoplay from "embla-carousel-autoplay"
+import Autoplay from 'embla-carousel-autoplay'
 
 export function UserCarousel() {
   return (
     <Carousel
       className="w-full"
-      opts={{ align: "start", loop: true}}
+      opts={{ align: 'start', loop: true }}
       plugins={[
         Autoplay({
           delay: 4000,
         }),
-      ]}>
+      ]}
+    >
       <CarouselContent>
         <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-1">
             <Card>
               <CardContent className="flex items-center justify-center p-6">
-                <span className="text-3xl font-semibold">
-                  1
-                </span>
+                <span className="text-3xl font-semibold">1</span>
               </CardContent>
             </Card>
           </div>
@@ -31,9 +36,7 @@ export function UserCarousel() {
           <div className="p-1">
             <Card>
               <CardContent className="flex items-center justify-center p-6">
-                <span className="text-3xl font-semibold">
-                  2
-                </span>
+                <span className="text-3xl font-semibold">2</span>
               </CardContent>
             </Card>
           </div>
@@ -42,9 +45,7 @@ export function UserCarousel() {
           <div className="p-1">
             <Card>
               <CardContent className="flex items-center justify-center p-6">
-                <span className="text-3xl font-semibold">
-                  3
-                </span>
+                <span className="text-3xl font-semibold">3</span>
               </CardContent>
             </Card>
           </div>
@@ -53,9 +54,7 @@ export function UserCarousel() {
           <div className="p-1">
             <Card>
               <CardContent className="flex items-center justify-center p-6">
-                <span className="text-3xl font-semibold">
-                  4
-                </span>
+                <span className="text-3xl font-semibold">4</span>
               </CardContent>
             </Card>
           </div>
@@ -64,9 +63,7 @@ export function UserCarousel() {
           <div className="p-1">
             <Card>
               <CardContent className="flex items-center justify-center p-6">
-                <span className="text-3xl font-semibold">
-                  5
-                </span>
+                <span className="text-3xl font-semibold">5</span>
               </CardContent>
             </Card>
           </div>
--- frontend/src/components/user/user-products.tsx
+++ frontend/src/components/user/user-products.tsx
@@ -1,20 +1,20 @@
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { UserCarousel } from "./user-carousel";
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { UserCarousel } from './user-carousel'
 
-export function UserProducts () {
+export function UserProducts() {
   return (
     <section className="flex flex-col items-center w-full my-5">
-          <div className="flex items-center justify-between w-full border-b border-zinc-300/20 pb-2 mb-5">
-            <div className="flex items-center gap-2 cursor-pointer">
-              <Avatar>
-                <AvatarImage src="https://github.com/duhnunes.png" />
-                <AvatarFallback>US</AvatarFallback>
-              </Avatar>
-              DuHNunes
-            </div>
-            Estrelas
-          </div>
-            <UserCarousel />
-        </section>
+      <div className="flex items-center justify-between w-full border-b border-zinc-300/20 pb-2 mb-5">
+        <div className="flex items-center gap-2 cursor-pointer">
+          <Avatar>
+            <AvatarImage src="https://github.com/duhnunes.png" />
+            <AvatarFallback>US</AvatarFallback>
+          </Avatar>
+          DuHNunes
+        </div>
+        Estrelas
+      </div>
+      <UserCarousel />
+    </section>
   )
 }
